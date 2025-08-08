0d2e96545e3429424002e4eb20f6ddceb07f2022
theannoyinglist <nilzmail11@gmail.com>
2025-08-08 08:53:25 +0200

    Update List.js

--- js/pages/List.js
+++ js/pages/List.js
@@ -104,7 +104,7 @@ export default {
                     </template>
                     <h3>Submission Requirements</h3>
                     <p>
-                        Achieved the record without using hacks. (however, FPS/Physics bypass is allowed, up to 480fps, a top 5 verification is exempt from the limit.)
+                        Achieved the record without using hacks. (however, FPS/Physics bypass is allowed, there is no limit.)
                     </p>
                     <p>
                         Achieved the record on the level that is listed on the site - please check the level ID before you submit a record.
